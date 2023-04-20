edfdebbdddf56361bc236c15c1bfb6c00efa8634
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-04-20 15:44:30 -0300

    formatting

--- task_api/urls.py
+++ task_api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from task_api.views import Tasks, TaskDetail
 
-urlpatterns = [path("", Tasks.as_view()), path("<str:pk>", TaskDetail.as_view())]
+urlpatterns = [path("", Tasks.as_view()),
+               path("<str:pk>", TaskDetail.as_view())]
